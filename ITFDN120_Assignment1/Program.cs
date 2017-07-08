ae6f9eb65709536b5c4d634fd6b5aa78e1bf204c
David Clayton <clayton@naturalselectionmusic.com>
2017-07-08 16:02:56 -0700

    First Try

--- ITFDN120_Assignment1/Program.cs
+++ ITFDN120_Assignment1/Program.cs
@@ -6,10 +6,50 @@ using System.Threading.Tasks;
 
 namespace ITFDN120_Assignment1
 {
-    class Program
+    class URLShortener
     {
+        public static char[] letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
+            'u','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N',
+                'O','P','Q','U','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'};
+
+        public static int GetIndex(char letter)
+        {
+            for(int i = 0; i<letters.Length; i++)
+            {
+                if (letters[i] == letter) return i;
+            }
+
+            return 0;
+        }
+
+        public static int Encode(string input)
+        {
+            int i = 0;
+            int encoded=0;
+
+            while(i<input.Length)
+            {
+                encoded += GetIndex(input[i]) * 10 * (input.Length - i);
+                i++;
+            }
+
+            return encoded;
+        }
         static void Main(string[] args)
         {
+            string url = "LpuPe81bc2w";
+            string shortenedURL = "";
+            
+            int encoded = Encode(url);
+
+            while(encoded>0)
+            {
+                shortenedURL = GetIndex((char)(encoded % 62)) + shortenedURL;
+                encoded = encoded / 62;
+            }
+
+            Console.WriteLine(shortenedURL);
+            Console.ReadLine();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # ITFDN120_Assignment1
+
+Problem:
+
 After watching the binary number video, did you notice the YouTube url?
 
 https://www.youtube.com/watch?v=LpuPe81bc2w
@@ -7,3 +10,10 @@ Write the code to convert the base-62 number to base-10.
 Submit your program (C#, Python, Javascript, ...) as well as the decimal number value for LpuPe81bc2w.
 
 Hint: Use an array/string that contains all the numbers and digits: "0123456789AB...YZab...yz"
+
+
+Notes:
+
+We want a bi-directional mapping of a given string using a Base 62 encoding schema.
+This is 62 letters: a-z A-Z 0-9.
+Use the modulus from diving by 62, then divide by 62, and repeat.
